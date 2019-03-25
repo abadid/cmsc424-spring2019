802a4f6987601c13f5e1af5bb889df7890181e0b
affan98 <affan98@gmail.com>
2019-03-24 23:17:54 -0400

    removed contents of file

--- project2/grading/queries.py
+++ project2/grading/queries.py
@@ -2,33 +2,11 @@ queries = ["" for i in range(0, 3)]
 
 ### 1.
 queries[1] = """
-SELECT customerid
-FROM customers NATURAL LEFT OUTER JOIN (
-    SELECT customerid, flightdate 
-    FROM flewon 
-    WHERE flightdate = date '2016-08-03'
-) as flewondate
-WHERE flightdate is NULL;
+
 """
 
 
 ### 2.
 queries[2] = """
-WITH allflights (airport, airlineid) AS (
-    SELECT source, airlineid FROM flights
-    UNION ALL
-    SELECT dest, airlineid FROM flights
-), totalflights (airportid, total) AS (
-    SELECT airport, COUNT(*)
-    FROM allflights
-    GROUP BY airport
-), swflights (airportid, sw) AS (
-    SELECT airport, COUNT(*)
-    FROM allflights
-    WHERE airlineid = 'SW'
-    GROUP BY airport
-)
-SELECT name, round(  COALESCE(sw, 0.00) / total    ,2) AS participation
-FROM swflights NATURAL RIGHT OUTER JOIN totalflights NATURAL JOIN airports
-ORDER BY participation DESC;
+
 """
